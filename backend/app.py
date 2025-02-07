e6f8e67b2f7a7df11abbaed1b2ba073f4cfc0b5b
IT21155970 <it21155970@my.sliit.lk>
2025-02-07 09:20:12 +0530

    app.py update  it21155970

--- backend/app.py
+++ backend/app.py
@@ -166,7 +166,6 @@ def forecast():
                     "district": district,
                     "week": week
                 })
-
                 # If no existing record, insert the new record
                 if not existing:
                     collection1.insert_one({
@@ -175,16 +174,11 @@ def forecast():
                         "week": week,
                         "demand_value": float(predictions[i])
                     })
-
         # Render results in the template
         return render_template('demand_prediction.html', results=results, weeks=weeks)
 
     except Exception as e:
         return str(e), 500
-
-
-
-    
 
 
 # Dummy user credentials---------------------------------------------------------------------------------------
